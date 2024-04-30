cad9fe140e8484da061acd5c2098c76ddf73403a
thattechgirlie <wanji.njoro1@gmail.com>
2024-05-01 00:06:51 +0300

    tasks

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,7 @@
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
