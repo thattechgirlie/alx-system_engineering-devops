ff68af840817ca85baaa54f7a802755c2991fd98
thattechgirlie <wanji.njoro1@gmail.com>
2024-06-28 09:39:37 +0300

    tasks

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""queries Reddit API, returns
-   list containing  titles of all hot articles for a given subreddit. """
+"""recursive function that queries the Reddit API and returns
+ a list containing the titles of all hot articles for a given subreddit. """
 
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-        """queries Reddit API, returns
-        list containing  titles of all hot articles for a given subreddit. """
-        url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-        headers = {
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-        params = {
+    params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-        response = requests.get(url, headers=headers, params=params,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-        if response.status_code == 404:
+    if response.status_code == 404:
         return None
 
-        results = response.json().get("data")
+    results = response.json().get("data")
     after = results.get("after")
     count += results.get("dist")
     for c in results.get("children"):
